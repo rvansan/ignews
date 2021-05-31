9512e3b9c06d40be77b35f78d01917ca7e0306fb
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-05-31 20:19:39 -0300

    03- Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubsctription } from "./_lib/menageSubscription";
 
 async function buffer(readable:Readable) {
     const chunks = [];
@@ -42,7 +43,25 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
         const { type } = event;
 
         if(relevantEvents.has(type)){
-            console.log('Evento recebido', event);
+            try {
+                switch(type) {
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;    
+                        await saveSubsctription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        );
+
+
+                        break;
+                    default:
+                        throw new Error('Unhandled event.')
+                }
+            }
+            catch(err){
+                return res.status(200).json({error: 'Webhook handler failed.'})
+            }
+            
         }
         
         res.status(200).json({ received: true});
